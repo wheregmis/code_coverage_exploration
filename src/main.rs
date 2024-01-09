4156e5c615fb5cdbfd96b1c57cd18667025f02e8
Sabin Regmi <get2sabin@gmail.com>
2024-01-09 11:00:15 -0500

    Added Initial Testing

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+}
